a977ce1ab578cf9df2ed74ebccbdb6ac02d70ee2
Marcelo Bauer <bauersaba@gmail.com>
2022-10-10 20:08:35 -0300

    [CARMINA]-release stable and working but MVP

--- carmina/test/widget_test.dart
+++ carmina/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:carmina/pages/firstpag.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carmina/main.dart';
+import 'package:carmina/main_origin.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
