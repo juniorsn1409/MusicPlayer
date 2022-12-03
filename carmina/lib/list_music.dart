6c2a5a06c2234a86c9f5a5b5b9f87f59cccb888c
Marcelo Bauer <bauersaba@gmail.com>
2022-12-03 19:03:55 -0300

    [CARMINA]others files

---
+++ carmina/lib/list_music.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListMusic extends StatefulWidget {
+  const ListMusic({Key? key}) : super(key: key);
+
+  @override
+  _ListMusicState createState() => _ListMusicState();
+}
+
+class _ListMusicState extends State<ListMusic> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
